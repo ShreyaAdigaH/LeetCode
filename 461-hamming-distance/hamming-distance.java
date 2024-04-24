e6d4d5f02ae525f8a732dd18f555ed0c683e3b05
Adiga Shreya <119755599+ShreyaAdigaH@users.noreply.github.com>
2024-04-24 16:34:25 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (18.39%) - LeetSync

--- 461-hamming-distance/hamming-distance.java
+++ 461-hamming-distance/hamming-distance.java
@@ -1,18 +1,12 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-                int dis = 0;
-
+               int dis = 0;
         int xor = x ^ y;
-
-        if(xor % 2 != 0) {
-            dis++;
-        }
-        for(int count = 0; count <= 32; count++) {
-            xor = xor >> 1;
-
-            if(xor % 2 != 0) {
+        for (int count = 0; count <= 32; count++) {
+            if (xor % 2 != 0) {
                 dis++;
             }
+            xor = xor >> 1;
         }
 
         return dis;
