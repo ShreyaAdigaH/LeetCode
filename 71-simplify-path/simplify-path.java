90468dac9ae2806bf3871fecce019cb7b3a9eea8
Adiga Shreya <119755599+ShreyaAdigaH@users.noreply.github.com>
2024-05-11 17:35:40 +0530

    Time: 4 ms (84.05%) | Memory: 44.5 MB (22.15%) - LeetSync

--- 71-simplify-path/simplify-path.java
+++ 71-simplify-path/simplify-path.java
@@ -1,32 +1,32 @@
 class Solution {
     public String simplifyPath(String path) {
-        int length = path.length();
+        char[] pathArray = path.toCharArray();
         StringBuilder result = new StringBuilder("/");
         Stack<String> resultStack = new Stack<>();
 
-       for(int idx = 1; idx < length; idx++) {
-           StringBuilder temp = new StringBuilder();
-           while(idx != length && path.charAt(idx) != '/') {
-               temp.append(path.charAt(idx));
-               idx++;
-           }
+        for (int idx = 1; idx < pathArray.length; idx++) {
+            StringBuilder temp = new StringBuilder();
+            while (idx != pathArray.length && pathArray[idx] != '/') {
+                temp.append(pathArray[idx]);
+                idx++;
+            }
 
-           if(!temp.toString().equals(".") && !temp.toString().equals("..") && !temp.toString().isEmpty()) {
-               resultStack.push(temp.reverse().toString());
-           }
+            if (!temp.toString().equals(".") && !temp.toString().equals("..") && !temp.toString().isEmpty()) {
+                resultStack.push(temp.reverse().toString());
+            }
 
-           if(!resultStack.empty() && temp.toString().equals("..")) {
-               resultStack.pop();
-           }
-       }
+            if (!resultStack.empty() && temp.toString().equals("..")) {
+                resultStack.pop();
+            }
+        }
 
-       while(!resultStack.empty()) {
-           result.append(resultStack.pop()).append("/");
-       }
+        while (!resultStack.empty()) {
+            result.append(resultStack.pop()).append("/");
+        }
 
-       if(result.length() == 1) {
-         return result.reverse().toString();
-       }
-       return result.deleteCharAt(0).reverse().toString();
+        if (result.length() == 1) {
+            return result.reverse().toString();
+        }
+        return result.deleteCharAt(0).reverse().toString();
     }
 }
