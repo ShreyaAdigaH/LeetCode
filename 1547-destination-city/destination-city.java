19972ac1cfd5e4cf0ea6f3c577a98a8c3d82c125
Adiga Shreya <119755599+ShreyaAdigaH@users.noreply.github.com>
2024-05-12 18:58:50 +0530

    Time: 3 ms (38.24%) | Memory: 43.1 MB (66.27%) - LeetSync

---
+++ 1547-destination-city/destination-city.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+         HashMap<String, String> srcDest = new HashMap<>();
+        for(List<String> cities : paths) {
+            srcDest.put(cities.get(0), cities.get(1));
+            if(!srcDest.containsKey(cities.get(1))) {
+                srcDest.put(cities.get(1), null);
+            }
+        }
+
+        for(String source : srcDest.keySet()) {
+            if(srcDest.get(source) == null) {
+                return source;
+            }
+        }
+        return null;
+    }
+}
