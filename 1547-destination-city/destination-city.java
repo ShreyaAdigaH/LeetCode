2b9a7c526ca013f891476cb5457a2d13b855b7b6
Adiga Shreya <119755599+ShreyaAdigaH@users.noreply.github.com>
2024-05-12 19:03:35 +0530

    Time: 2 ms (99.69%) | Memory: 43 MB (76.95%) - LeetSync

--- 1547-destination-city/destination-city.java
+++ 1547-destination-city/destination-city.java
@@ -1,18 +1,31 @@
 class Solution {
     public String destCity(List<List<String>> paths) {
-         HashMap<String, String> srcDest = new HashMap<>();
-        for(List<String> cities : paths) {
-            srcDest.put(cities.get(0), cities.get(1));
-            if(!srcDest.containsKey(cities.get(1))) {
-                srcDest.put(cities.get(1), null);
-            }
+        // HashMap<String, String> srcDest = new HashMap<>();
+        // for(List<String> cities : paths) {
+        // srcDest.put(cities.get(0), cities.get(1));
+        // if(!srcDest.containsKey(cities.get(1))) {
+        // srcDest.put(cities.get(1), null);
+        // }
+        // }
+
+        // for(String source : srcDest.keySet()) {
+        // if(srcDest.get(source) == null) {
+        // return source;
+        // }
+        // }
+        // return null;
+
+        HashSet<String> sources = new HashSet<>();
+
+        for (List<String> cities : paths) {
+            sources.add(cities.get(0));
         }
 
-        for(String source : srcDest.keySet()) {
-            if(srcDest.get(source) == null) {
-                return source;
+        for (List<String> cities : paths) {
+            if (!sources.contains(cities.get(1))) {
+                return cities.get(1);
             }
         }
-        return null;
+        return "";
     }
 }
