7fb240fca82a718d50bcde96d31668f4437547ca
Adiga Shreya <119755599+ShreyaAdigaH@users.noreply.github.com>
2024-05-15 18:19:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (90.49%) - LeetSync

--- 263-ugly-number/ugly-number.java
+++ 263-ugly-number/ugly-number.java
@@ -5,14 +5,14 @@ class Solution {
         } if (n == 0) {
             return false;
         }
-        if(n % 2 == 0) {
-            return isUgly(n /2);
+        if(n % 5 == 0) {
+            return isUgly(n /5);
         }
         if(n % 3 == 0) {
             return isUgly(n / 3);
         }
-        if(n % 5 == 0) {
-            return isUgly(n / 5);
+        if(n % 2 == 0) {
+            return isUgly(n / 2);
         } 
             return false;
     }
