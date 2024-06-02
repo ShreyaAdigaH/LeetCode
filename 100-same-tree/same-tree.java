a1e5310d07704ec6b273796e46545e03665c892a
Adiga Shreya <119755599+ShreyaAdigaH@users.noreply.github.com>
2024-06-02 19:23:03 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (73.64%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,9 +15,16 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-          if(p == null && q == null) {
+                //each node in the tree is a root to the subtree below
+        // so just compare root values by recursively calling the function
+
+        if(p == null && q == null) {
             return true;
         }
+        
+        // compares the value of nodes in the same level
+        //if one node is smaller is than other then return false
+        // if the value of node at same level is not equal then return false
         if(p != null && q != null && p.val == q.val) {
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         } else {
