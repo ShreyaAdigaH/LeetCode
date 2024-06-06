5d8ad2a35376fed7ff417b1920dcd6d647d139f6
Adiga Shreya <119755599+ShreyaAdigaH@users.noreply.github.com>
2024-06-06 17:32:25 +0530

    Time: 9 ms (99.76%) | Memory: 44.6 MB (97.30%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-               if(hand.length % groupSize != 0) {
+        if (hand.length % groupSize != 0) {
             return false;
         }
 
         Arrays.sort(hand);
 
-        for(int index = 0; index < hand.length; index++) {
-            if(hand[index] >= 0) {
+        for (int index = 0; index < hand.length; index++) {
+            if (hand[index] >= 0) {
                 int currentIndex = index;
                 int cardCount = 1;
                 int successor = hand[index] + 1;
                 hand[index] = -1;
-                while(currentIndex < hand.length && cardCount < groupSize) {
-                    if(hand[currentIndex] == successor) {
+                while (currentIndex < hand.length && cardCount < groupSize) {
+                    if (hand[currentIndex] == successor) {
                         hand[currentIndex] = -1;
                         successor = successor + 1;
                         cardCount++;
@@ -22,7 +22,7 @@ class Solution {
                     }
                     currentIndex++;
                 }
-                if(cardCount != groupSize)
+                if (cardCount != groupSize)
                     return false;
             }
         }
