5a7ef26c934670e124a17efff0251b61a95ed519
Adiga Shreya <119755599+ShreyaAdigaH@users.noreply.github.com>
2024-06-07 17:55:24 +0530

    Time: 10 ms (88.52%) | Memory: 55.1 MB (69.48%) - LeetSync

--- 648-replace-words/replace-words.java
+++ 648-replace-words/replace-words.java
@@ -1,25 +1,58 @@
 class Solution {
+    Trie root;
+
     public String replaceWords(List<String> dictionary, String sentence) {
-        HashSet<String> dict = new HashSet<>(dictionary);
-        StringBuilder result = new StringBuilder();
-        StringBuilder temp;
-        for (int index = 0; index < sentence.length(); index++) {
-            temp = new StringBuilder();
-            boolean wordFormed = false;
-            while (index < sentence.length() && sentence.charAt(index) != ' ') {
-                if (!wordFormed) {
-                    temp.append(sentence.charAt(index));
-                    if (dict.contains(temp.toString())) {
+        root = new Trie();
 
-                        wordFormed = true;
-                    }
-                }
-                index++;
-            }
-            result.append(temp).append(" ");
-
+        for (String word : dictionary) {
+            constructTries(word);
         }
-        return result.deleteCharAt(result.length() - 1).toString();
+        StringBuilder result = new StringBuilder();
+        String[] input = sentence.split(" ");
+        for (String word : input) {
+            result.append(searchPrefixInTrie(word));
+            result.append(" ");
+        }
+        return result.toString().trim();
+    }
 
+    public String searchPrefixInTrie(String word) {
+        Trie node = root;
+        char[] letters = word.toCharArray();
+        for (int index = 0; index < letters.length; index++) {
+            int letter = letters[index] - 'a';
+            if (node.children[letter] == null) {
+                return word;
+            }
+            if (node.children[letter].isEnd) {
+                return word.substring(0, index + 1);
+            }
+            node = node.children[letter];
+        }
+        return word;
+    }
+
+    public void constructTries(String word) {
+        Trie node = root; // have one common root for all the words in dictionary.
+
+        char[] letters = word.toCharArray();
+        for (char letter : letters) {
+            int character = letter - 'a';
+            if (node.children[character] == null) { // check if current node has a child representing given letter
+                node.children[character] = new Trie(); // if not add a Trie node for that character
+            }
+            node = node.children[character];
+        }
+        node.isEnd = true;
+    }
+}
+
+public class Trie {
+    public Trie[] children;
+    public boolean isEnd;
+
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
     }
 }
