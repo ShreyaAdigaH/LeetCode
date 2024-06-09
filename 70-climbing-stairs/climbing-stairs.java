e416529c6c1cf0d216e79bc66b3b70d1db48ae4b
Adiga Shreya <119755599+ShreyaAdigaH@users.noreply.github.com>
2024-06-09 19:13:17 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (84.24%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,6 +1,7 @@
 class Solution {
     int[] stairs;
     public int climbStairs(int n) {
+        /**
        stairs = new int[n + 1];
         // Number of ways to reach nth step = Number of ways to reach (n-1)th step + Number of ways to reach (n-2)th step
         // because you can reach come to nth step either from n-1th step or (n-2)th step
@@ -19,5 +20,18 @@ class Solution {
 
         }
         return stairs[n];
+        **/
+
+         if(n == 1 || n == 0) 
+            return 1;
+        
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int stair = 3; stair <= n; stair++) {
+            dp[stair] = dp[stair - 1] + dp[stair - 2];
+        }
+        return dp[n];
     }
 }
