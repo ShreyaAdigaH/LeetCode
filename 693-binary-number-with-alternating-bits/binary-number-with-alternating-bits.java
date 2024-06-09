11e5cf5b0d77d486e94596267d58a87fb768bcca
Adiga Shreya <119755599+ShreyaAdigaH@users.noreply.github.com>
2024-06-09 17:12:12 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (98.05%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.java
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.java
@@ -10,3 +10,7 @@ class Solution {
         return true;
     }
 }
+
+// (n & 1) ^ (n >> 1) -- if it has alternating bits then number and number when left shifted 1 times should be different
+//                    -- in that case XORing the 2 numbers should give us 1
+// (((n & 1) ^ (n >> 1)) & 1) -- after XORing read the last bit using & operator
