7520cfefb62acc83c483d688cb549c5bcba962da
Adiga Shreya <119755599+ShreyaAdigaH@users.noreply.github.com>
2024-07-10 11:45:24 +0530

    Time: 1 ms (99.42%) | Memory: 41.9 MB (67.59%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.java
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -1,12 +1,24 @@
 class Solution {
     public int minOperations(String[] logs) {
-        int level = 0;
-        for (String dir : logs) {
-            if (dir.equals("../")) {
-                if (level > 0) {
+        // int level = 0;
+        // for (String dir : logs) {
+        //     if (dir.equals("../")) {
+        //         if (level > 0) {
+        //             level -= 1;
+        //         }
+        //     } else if (!dir.equals("./")) {
+        //         level += 1;
+        //     }
+        // }
+        // return level;
+
+                int level = 0;
+        for(String dir : logs) {
+            if(dir.charAt(1) == '.') {
+                if(level > 0) {
                     level -= 1;
                 }
-            } else if (!dir.equals("./")) {
+            } else if(dir.charAt(0) != '.') {
                 level += 1;
             }
         }
