bdd0eadeedecb27736444c7c148332a5ad3cb004
Adiga Shreya <119755599+ShreyaAdigaH@users.noreply.github.com>
2024-07-27 00:51:31 +0530

    Time: 1 ms (93.58%) | Memory: 42.7 MB (57.43%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.java
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -3,25 +3,22 @@ class Solution {
         int freq[] = new int[nums.length + 1];
         int max = nums.length;
 
-        if(nums.length == 1) {
-           return false;
+        if (nums.length == 1) {
+            return false;
         }
-        for(int index = 0; index < nums.length; index++) {
-            if(nums[index] > nums.length) {
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] > nums.length) {
                 return false;
-            } 
+            }
             freq[nums[index]]++;
         }
 
-        for(int index = 1; index <= nums.length - 1; index++) {
-            if(index == nums.length - 1) {
-                 if(freq[index] != 2) {
-                    return false;
-                 }
-            } else {
-                 if(freq[index] != 1) {
-                    return false;
-                 }
+        if (freq[nums.length - 1] != 2) {
+            return false;
+        }
+        for (int index = 1; index <= nums.length - 2; index++) {
+            if (freq[index] != 1) {
+                return false;
             }
         }
         return true;
