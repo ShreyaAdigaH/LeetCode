e4cec926b34a0675fa6268f052b62115328bad82
Adiga Shreya <119755599+ShreyaAdigaH@users.noreply.github.com>
2024-09-30 20:08:46 +0530

    Time: 5 ms (93.47%) | Memory: 45.7 MB (42.51%) - LeetSync

--- 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.java
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.java
@@ -1,25 +1,30 @@
 class CustomStack {
 
    int[] arrStack;
+      int[] incrementArr;
     int size = -1;
     int maxSize = 0;
     public CustomStack(int maxSize) {
-        arrStack = new int[maxSize];
+       arrStack = new int[maxSize];
+        incrementArr = new int[maxSize];
         this.maxSize = maxSize;
-        Arrays.fill(arrStack, -1);
     }
 
     public void push(int x) {
         if(size < maxSize - 1) {
             arrStack[++size] = x;
         }
-    
+    //    System.out.println(STR."Pushed \{Arrays.toString(arrStack)}");
     }
 
     public int pop() {
         if(size >= 0) {
-            int val = arrStack[size];
-            arrStack[size--] = -1;
+            int val = arrStack[size] + incrementArr[size];
+            if(size > 0) {
+                incrementArr[size - 1] += incrementArr[size];
+            }
+            incrementArr[size] = 0;
+            size--;
             return val;
         } else {
             return -1;
@@ -27,10 +32,14 @@ class CustomStack {
     }
 
     public void increment(int k, int val) {
-        for(int index = 0; index <= size && index < k && arrStack[index] != -1; index++) {
-            arrStack[index] = arrStack[index] + val;
+//        for(int index = 0; index <= size && index < k && arrStack[index] != -1; index++) {
+//            arrStack[index] = arrStack[index] + val;
+//        }
+    //    System.out.println(STR."Incremented \{Arrays.toString(arrStack)}");
+        if (size >= 0) {
+            int limit = Math.min(k, size + 1);
+            incrementArr[limit - 1] += val; 
         }
-
 
     }
 }
