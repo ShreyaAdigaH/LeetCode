3c246e553383dffbb3e9405c66f7258c8172f924
Adiga Shreya <119755599+ShreyaAdigaH@users.noreply.github.com>
2024-10-15 23:31:58 +0530

    Time: 8 ms (45.52%) | Memory: 45.2 MB (86.38%) - LeetSync

---
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long minimumSteps(String s) {
+        int zeroCount = 0;
+        long count = 0;
+
+        for(int i = s.length() - 1; i >= 0; i--) {
+            if(s.charAt(i) == '0') {
+                zeroCount++;
+            } else {
+                        count += zeroCount;
+            }
+        }
+        return count;
+    }
+}
