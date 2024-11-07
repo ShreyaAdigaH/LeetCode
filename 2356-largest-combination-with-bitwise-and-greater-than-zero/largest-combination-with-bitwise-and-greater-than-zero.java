537bb81d91ba6ea0890c8eb62ba3c678a6b77872
Adiga Shreya <119755599+ShreyaAdigaH@users.noreply.github.com>
2024-11-07 21:27:01 +0530

    Time: 35 ms (15.87%) | Memory: 56.4 MB (72.91%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.java
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.java
@@ -1,11 +1,12 @@
 class Solution {
     public int largestCombination(int[] candidates) {
           int maxLen = 0;
-
+          int count = 0;
+          int bit = 0;
             for(int i = 0; i <= 32; i++) {
-                int count = 0;
+                count = 0;
                 for(int candidate : candidates) {
-                    int bit = (1 << i) & candidate;
+                    bit = (1 << i) & candidate;
                     if(bit != 0) {
                         count++;
                     }
