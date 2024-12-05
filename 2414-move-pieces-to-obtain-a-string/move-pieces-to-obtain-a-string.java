20490908a5affd6c7491c3aa28e88f60353167d8
Adiga Shreya <119755599+ShreyaAdigaH@users.noreply.github.com>
2024-12-05 21:34:23 +0530

    Time: 42 ms (30.59%) | Memory: 45.5 MB (71.35%) - LeetSync

---
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean canChange(String start, String target) {
+        if (!start.replace("_", "").equals(target.replace("_", ""))) {
+            return false;
+        }
+        int n = start.length();
+        int i = 0, j = 0;
+
+        while (i < n && j < n) {
+            while (i < n && start.charAt(i) == '_') {
+                i++;
+            }
+            while (j < n && target.charAt(j) == '_') {
+                j++;
+            }
+            if (i < n && j < n) {
+                char c1 = start.charAt(i);
+                char c2 = target.charAt(j);
+                if (c1 != c2) {
+                    return false;
+                }
+                if (c1 == 'L' && i < j) {
+                    return false;
+                }
+                if (c1 == 'R' && i > j) {
+                    return false;
+                }
+            }
+
+            i++;
+            j++;
+        }
+
+        return true;
+    }
+}
