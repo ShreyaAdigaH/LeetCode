51b06c66ba6d7b186e6765bd9192b5131000a575
QuantumNeuron <119755599+ShreyaAdigaH@users.noreply.github.com>
2025-08-09 17:45:19 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (27.57%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,37 +1,24 @@
 class Solution {
-    int[] stairs;
+    int[] map = new int[46];
+
     public int climbStairs(int n) {
-        /**
-       stairs = new int[n + 1];
-        // Number of ways to reach nth step = Number of ways to reach (n-1)th step + Number of ways to reach (n-2)th step
-        // because you can reach come to nth step either from n-1th step or (n-2)th step
-        return calculateSteps(n); 
-    }
-
-    public int calculateSteps(int n) {
-        if(stairs[n] == 0) {
-            if (n == 1)
+        // for nth step once you reach step (n - 1) and (n - 2) you can hop over
+        // base case 0th step (ground level) and 1st step - 1 way to reach
+        if (map[n] == 0) {
+            if (n == 0 || n == 1) {
+                map[n] = 1;
                 return 1;
+            }
 
-            if (n == 2)
-                return 2;
-
-            stairs[n] = calculateSteps(n - 1) + calculateSteps(n - 2);
-
+          //  int ways = climbStairs(n - 1) + climbStairs(n - 2);
+            int ways = 0;
+            for(int i = 1; i <= 2; i ++) {
+                ways += climbStairs(n - i);
+            }
+            map[n] = ways;
+            return ways;
+        } else {
+            return map[n];
         }
-        return stairs[n];
-        **/
-
-         if(n == 1 || n == 0) 
-            return 1;
-        
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-
-        for(int stair = 3; stair <= n; stair++) {
-            dp[stair] = dp[stair - 1] + dp[stair - 2];
-        }
-        return dp[n];
     }
 }
