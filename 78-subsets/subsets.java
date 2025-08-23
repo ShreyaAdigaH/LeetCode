1e1d48de2b12fcb5c45f089decbefbf6dbd64e01
QuantumNeuron <119755599+ShreyaAdigaH@users.noreply.github.com>
2025-08-23 21:58:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (46.33%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,37 +1,22 @@
 class Solution {
-    /**
-     * Code works by adding the subsets of below in the given order
-     * 1) 0th element
-     * 2) 0th 1st element
-     * 3) 0th 1st 2nd element
-     * .
-     * .
-     * .
-     * n) 0th 1st 2nd ....(n - 1)th element
-     * and then backtrack to respective subsets to check other possible combination with it
-     */
-
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(nums, 0, new ArrayList<>(), result);
-        return result;
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        generateSubsets(0, nums, curr, res);
+        return res;
     }
-    /**
-     * Order of addition to the result
-     * stand at an element and check all possible combinations of subset with it in forward direction
-     * Stand at {1} and call recursion to check possible subsets while standing at 1
-     * 1st possible subset {1, 2}
-     * stand at 2 and call recursion to check possible subsets while standing at 2
-     * 2nd possible subset {1, 2, 3}
-     **/
 
-    public void backtrack(int[] nums, int start, List<Integer> subset, List<List<Integer>> result) {
-        result.add(new ArrayList<>(subset));
-        for (int i = start; i < nums.length; i++) {
-            subset.add(nums[i]);
-            backtrack(nums, i + 1, subset, result);
-            subset.removeLast(); // because you have to remove the last added element to the list to check
-                               // for the possible subsets with the list before adding the last element
+    public void generateSubsets(int i, int[] nums, List<Integer> curr, List<List<Integer>> res) {
+        //base case 
+        if(i == nums.length) {
+            res.add(new ArrayList<>(curr));
+            return;
         }
+
+        curr.add(nums[i]);
+        generateSubsets(i + 1, nums, curr, res);
+
+        curr.remove(curr.size() - 1);
+        generateSubsets(i + 1, nums, curr, res);
     }
 }
