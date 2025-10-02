67edf069a468541425525565f82baf5eb28eca89
QuantumNeuron <119755599+ShreyaAdigaH@users.noreply.github.com>
2025-10-02 14:58:08 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (77.79%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -2,18 +2,30 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
       // horizontal scanning
 
-      String currPrefix = strs[0];
+    //   String currPrefix = strs[0];
 
+    //   for(int i = 1; i < strs.length; i++) {
+    //     String curr = strs[i];
+    //     int j = 0;
+    //     //update prefix
+    //     while(j < curr.length() && j < currPrefix.length() && currPrefix.charAt(j) == curr.charAt(j)) {
+    //         j++;
+    //     }
+    //     currPrefix = currPrefix.substring(0, j);
+    //   }
+
+    //   return currPrefix;
+    // }
+
+
+      String currPrefix = strs[0];
       for(int i = 1; i < strs.length; i++) {
-        String curr = strs[i];
-        int j = 0;
-        //update prefix
-        while(j < curr.length() && j < currPrefix.length() && currPrefix.charAt(j) == curr.charAt(j)) {
-            j++;
+        while(strs[i].indexOf(currPrefix) != 0) {
+            currPrefix = currPrefix.substring(0, currPrefix.length() - 1);
         }
-        currPrefix = currPrefix.substring(0, j);
       }
 
       return currPrefix;
     }
+
 }
