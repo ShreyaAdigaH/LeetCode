65ca527ca38e0b335883f0aae967feaf8781292b
QuantumNeuron <119755599+ShreyaAdigaH@users.noreply.github.com>
2025-10-28 20:30:55 +0530

    Time: 12 ms (53.29%) | Memory: 45.5 MB (25.26%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
@@ -1,14 +1,9 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
         int lasers = 0;
-        for (int i = 0; i < bank[0].length(); i++) {
-            if (bank[0].charAt(i) == '1')
-                lasers += 1;
-        }
-
         int beamCount = 0;
         int prev = lasers;
-        for (int i = 1; i < bank.length; i++) {
+        for (int i = 0; i < bank.length; i++) {
             int curr = 0;
             for (int j = 0; j < bank[i].length(); j++) {
                 if (bank[i].charAt(j) == '1')
