4655084635d5e372d38cf2f47dca695eb2405904
QuantumNeuron <119755599+ShreyaAdigaH@users.noreply.github.com>
2025-10-28 20:13:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (33.73%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
@@ -1,39 +1,64 @@
 class Solution {
     public int countValidSelections(int[] nums) {
-    
-        int nonZeros = 0;
-        int count = 0;
 
-        for(int num : nums) {
-            if(num > 0)
-                nonZeros++;
+        //     int nonZeros = 0;
+        //     int count = 0;
+
+        //     for(int num : nums) {
+        //         if(num > 0)
+        //             nonZeros++;
+        //     }
+
+        //     for(int i = 0; i < nums.length; i++) {
+        //         if(nums[i] == 0) {
+        //             if(isValid(nums, i, 1, nonZeros))
+        //                 count++;
+        //             if(isValid(nums, i, -1, nonZeros))
+        //                 count++;
+        //         }
+        //     }
+        //     return count;
+        // }
+
+        // public boolean isValid(int[] nums, int start, int dir, int nonZeros) {
+        //     int[] temp = nums.clone();
+        //     int curr = start;
+        //     int n = nums.length;
+
+        //     while(nonZeros > 0 && curr < n && curr >= 0) {
+        //         if(temp[curr] > 0) {
+        //             temp[curr]--;
+        //             dir = dir * -1;
+        //             if(temp[curr] == 0)
+        //                 nonZeros--;
+        //         }
+        //             curr += dir;
+        //     }
+        //     return nonZeros == 0;
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for (int num : nums) {
+            rightSum += num;
         }
 
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] == 0) {
-                if(isValid(nums, i, 1, nonZeros))
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                if (leftSum - rightSum >= 0 && leftSum - rightSum <= 1) {
                     count++;
-                if(isValid(nums, i, -1, nonZeros))
+                }
+
+                if (rightSum - leftSum >= 0 && rightSum - leftSum <= 1) {
                     count++;
+                }
+            } else {
+                leftSum += nums[i];
+                rightSum -= nums[i];
             }
         }
         return count;
-    }
-
-    public boolean isValid(int[] nums, int start, int dir, int nonZeros) {
-        int[] temp = nums.clone();
-        int curr = start;
-        int n = nums.length;
-
-        while(nonZeros > 0 && curr < n && curr >= 0) {
-            if(temp[curr] > 0) {
-                temp[curr]--;
-                dir = dir * -1;
-                if(temp[curr] == 0)
-                    nonZeros--;
-            }
-                curr += dir;
-        }
-        return nonZeros == 0;
     }
 }
