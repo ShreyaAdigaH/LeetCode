10208eb99e8ffed0e00e5e941c60aeaa4cca3931
QuantumNeuron <119755599+ShreyaAdigaH@users.noreply.github.com>
2025-10-28 19:59:07 +0530

    Time: 132 ms (6.63%) | Memory: 43.2 MB (16.87%) - LeetSync

---
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int countValidSelections(int[] nums) {
+    
+        int nonZeros = 0;
+        int count = 0;
+
+        for(int num : nums) {
+            if(num > 0)
+                nonZeros++;
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) {
+                if(isValid(nums, i, 1, nonZeros))
+                    count++;
+                if(isValid(nums, i, -1, nonZeros))
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean isValid(int[] nums, int start, int dir, int nonZeros) {
+        int[] temp = nums.clone();
+        int curr = start;
+        int n = nums.length;
+
+        while(curr < n && curr >= 0) {
+            if(temp[curr] > 0) {
+                temp[curr]--;
+                dir = dir * -1;
+                if(temp[curr] == 0)
+                    nonZeros--;
+            }
+                curr += dir;
+        }
+        return nonZeros == 0;
+    }
+}
