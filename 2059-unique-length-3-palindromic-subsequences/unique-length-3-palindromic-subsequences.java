ceb339cff294d7eb7406e22beaf2e801c1f1c1b9
QuantumNeuron <119755599+ShreyaAdigaH@users.noreply.github.com>
2025-11-21 14:24:39 +0530

    Time: 1275 ms (5.16%) | Memory: 54.2 MB (5.17%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -1,21 +1,22 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-          int[] charFreq = new int[26];
-        for(int i = 2; i < s.length(); i++) {
-            charFreq[s.charAt(i) - 'a']++;
-        }
-        boolean[] charSet = new boolean[26];
-        charSet[s.charAt(0) - 'a'] = true;
+        int[] leftMap = new int[26];
+        int[] rightMap = new int[26];
         HashSet<String> res = new HashSet<>();
 
+        for(int i = 1; i < s.length(); i++) {
+            leftMap[s.charAt(i) - 'a']++;
+        }
+
         for(int i = 1; i < s.length() - 1; i++) {
+            rightMap[s.charAt(i - 1) - 'a']++;
+            leftMap[s.charAt(i) - 'a']--;
+
             for(int c = 0; c < 26; c++) {
-                if(charSet[c] && charFreq[c] > 0) {
-                     res.add("" + (char) (c + 'a') + s.charAt(i) + (char) (c + 'a'));
+                if(rightMap[c] > 0 && leftMap[c] > 0) {
+                   res.add("" + (char) (c + 'a') + s.charAt(i) + (char) (c + 'a'));
                 }
             }
-            charSet[s.charAt(i) - 'a'] = true;
-            charFreq[s.charAt(i + 1) - 'a']--;
         }
         return res.size();
     }
