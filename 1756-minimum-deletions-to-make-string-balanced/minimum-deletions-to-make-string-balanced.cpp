c601fed85d24a746fbe1f3044c2dea1eeab4b988
QuantumNeuron <119755599+ShreyaAdigaH@users.noreply.github.com>
2026-02-07 20:50:29 +0530

    Time: 28 ms (52.07%) | Memory: 26 MB (49.26%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n = s.length();
+       int n = s.length();
 
-        vector<int> a_count(n, 0);
-        vector<int> b_count(n, 0);
+       stack<char> charStack;
+       int deleteCount = 0;
 
-        // calculate prefix b
-
-        int b_s = 0;
-        for(int i = 0; i < n; i++) {
-            b_count[i] = b_s;
-            if(s[i] == 'b') 
-                b_s++;
+       for(int i = 0; i < n; i++) {
+        if(s[i] == 'b') {
+            charStack.push(s[i]);
+        } else {
+            if(!charStack.empty() && charStack.top() == 'b') {
+                charStack.pop();
+                deleteCount++;
+            }
         }
+       }
 
-        int a_s = 0;
-
-        for(int i = n - 1; i >= 0; i--) {
-            a_count[i] = a_s;
-            if(s[i] == 'a') 
-                a_s++;
-        }
-
-        int minOps = n;
-
-        for(int i = 0; i < n; i++) {
-            minOps = min(minOps, a_count[i] + b_count[i]);
-        }
-
-        return minOps;
+       return deleteCount;
     }
 };
